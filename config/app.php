dd38b6b6d3383b4b66eee07045ba3e62b1b8f57d
anil-p-gupta <anilpgupta@gmail.com>
2018-02-26 10:41:20 +0530

    jsvalidation removed

--- config/app.php
+++ config/app.php
@@ -178,7 +178,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Proengsoft\JsValidation\JsValidationServiceProvider::class,
 
     ],
 
@@ -230,7 +229,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'JsValidator' => Proengsoft\JsValidation\Facades\JsValidatorFacade::class,
 
     ],
 
