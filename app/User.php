b0a44fd97a786721ca83a32e63e9992c10bf8c0d
Sujit magdum <magdumsujit@gmail.com>
2018-05-06 19:30:44 +0530

    menu for personalization

--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
     public function verifyUser(){
         return $this->hasOne('App\Model\VerifyUser');
     }
-    
+
     /**
      * Get verified user id associated otp.
      */
