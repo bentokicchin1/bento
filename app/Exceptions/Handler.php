6a684acdd7afcd0d615399722f7ca795b42dbb08
Sujit magdum <magdumsujit@gmail.com>
2018-05-13 21:07:36 +0530

    general error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,8 +49,8 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-
-        return parent::render($request, $exception);
+        return view('errors.general_error');
+        // return parent::render($request, $exception);
     }
 
 
