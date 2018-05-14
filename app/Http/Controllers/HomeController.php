09f95b446f305bb64ca56a798f220fcda22e2edd
Sujit magdum <magdumsujit@gmail.com>
2018-05-14 22:26:39 +0530

    order type acc to billing type

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,10 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $userData = array();
-        if(Auth::user()){
-            $userData = Auth::user();
-        }
-        return view('home',['userData' => $userData]);
+        return view('home');
     }
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,70 +2,48 @@
   ================================================== -->
   <div class="nav-container">
     <nav class="nav-inner transparent">
-
       <div class="navbar">
         <div class="container">
           <!-- <div class="row"> -->
-
-            <div class="brand">
+          <div class="brand">
               <a  href="/">Bento</a>
           </div>
-
           <div class="navicon">
               <div class="menu-container">
-
                 <div class="circle dark inline">
                   <i class="icon ion-navicon"></i>
-              </div>
-              <div class="list-menu">
+                </div>
+                <div class="list-menu">
                   <i class="icon ion-close-round close-iframe"></i>
                   <div class="intro-inner">
-
                     <ul id="nav-menu">
                        @guest
                        <li><a href="{{ route('login') }}">Login</a></li>
                        <li><a href="{{ route('register') }}">Register</a></li>
                        <li><a href="{{ route('contact-us') }}">Contact-Us</a></li>
                        @else
-                       <li><a href="{{ route('profile') }}">My Account</a></li>
-                       <li><a href="{{ route('orders') }}">My Orders</a></li>
-                       <li ><a href="{{ route('home') }}">Order Now</a></li>
-                       <li><a href="{{ route('subscriptionType') }}">Personalize Your Order</a></li>
-                       <!-- @if(!empty($userData))
-                          @if($userData->billing_cycle=='daily')
-                            <li ><a href="{{ route('orders') }}">Order Now</a></li>
-                          @else
+                         <li><a href="{{ route('profile') }}">My Account</a></li>
+                         <li><a href="{{ route('orders') }}">My Orders</a></li>
+                         @if(Auth::user()->billing_cycle=='daily')
+                            <li ><a href="{{ route('home') }}">Order Now</a></li>
+                         @else
                             <li><a href="{{ route('subscriptionType') }}">Personalize Your Order</a></li>
-                          @endif
-                        @else -->
-                        <!-- @endif -->
+                         @endif
                        <li><a href="{{ route('feedback') }}">Feedback</a></li>
                        <li>
-                          <a href="{{ route('logout') }}"
-                          onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-                          Logout
-                      </a>
-
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                          {{ csrf_field() }}
-                      </form>
-                  </li>
-                  <li><a href="{{ route('contact-us') }}">Contact-Us</a></li>
-
-                  @endguest
-
-              </ul>
-
+                          <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                          </form>
+                        </li>
+                        <li><a href="{{ route('contact-us') }}">Contact-Us</a></li>
+                    @endguest
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
-
+    </nav>
   </div>
-</div>
-
-<!-- </div> -->
-</div>
-</div>
-
-</nav>
-</div>
