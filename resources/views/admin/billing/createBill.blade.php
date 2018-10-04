ea96dcc6efb076f61afcbe1edbc801cea9fe0670
Sujit magdum <magdumsujit@gmail.com>
2018-10-04 22:47:27 +0530

    create bill format

--- resources/views/admin/billing/createBill.blade.php
+++ resources/views/admin/billing/createBill.blade.php
@@ -45,7 +45,7 @@
                 <div class="col-sm-offset-3 col-sm-6">
                     <label for="name" class="col-sm-6 control-label">Enter Pending Bill Amount :</label>
                     <div class="col-sm-6">
-                      {{ Form::text('pendingAmount',0,null,['id'=>'pendingAmount','class'=>'form-control','placeholder'=>'Pending Bill Amount (required)']) }}
+                      {{ Form::text('pendingAmount',0,['id'=>'pendingAmount','class'=>'form-control','placeholder'=>'Pending Bill Amount (required)']) }}
                     </div>
                 </div>
               </div>
@@ -61,9 +61,9 @@
                             @foreach ($dates as $day)
                               <div class="form-group col-sm-3">
                                   <div class="col-sm-2">
-                                    {{ Form::checkbox('days[]',date('Y-m-d',strtotime($day)), true,[]) }}
+                                    {{ Form::checkbox('days[]',date('Y-m-d',strtotime($day)), true,['id'=>date('Y-m-d',strtotime($day))]) }}
                                   </div>
-                                  <label for="days[]" class="col-sm-9 control-label">{{date('l, d M',strtotime($day))}}</label>
+                                  <label for="{{date('Y-m-d',strtotime($day))}}" class="col-sm-9 control-label">{{date('l, d M',strtotime($day))}}</label>
                               </div>
                             @endforeach
                         </div>
@@ -83,30 +83,59 @@
         </div>
         <!-- /.col -->
       </div>
+      <div class="modal fade" id="billModal" tabindex="-1" role="dialog" aria-labelledby="billModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Your Bill Is Ready</h4>
+            </div>
+            <div class="modal-body"><div class="createdBill"></div></div>
+            <div class="modal-footer"></div>
+          </div>
+        </div>
+    </div>
       <!-- /.row -->
     </section>
     <script>
       var billAmount = 0;
       $( document ).ready(function() {
         var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        // var messageFormat = "<p>Hi,</p><p>Total tiffins count of September 2018 = [totalTiffin]</p><p>Veg Tiffins On ([vegTiffinDates])</p><p>Non Veg Tiffins On ([nonvegTiffinDates])</p><p>Previous unbilled amount = [pendingAmount]<br />Current unbilled amount = [totalBillAmount]<br />Total unbilled amount = [grandTotal]</p><p>Thanks and Regards<br />Bento</p>";
+
         $("#billformat").submit(function(e) {
+            var messageFormat = "<p>Hi,</p><p>Total tiffins count of September 2018 = [totalTiffin] </p><p>Veg Tiffins On ([vegTiffinDates])</p><p>Non Veg Tiffins On ([nonvegTiffinDates])</p><p>Previous unbilled amount = [pendingAmount] <br />Current unbilled amount = [totalBillAmount] <br />Total unbilled amount = [grandTotal] </p><p>Thanks and Regards<br />Bento</p>";
+
             e.preventDefault(); // avoid to execute the actual submit of the form.
             var form = $(this);
+            var formattedText = '';
+            var vegTiffinDates = [];var nonvegTiffinDates = [];
             var tiffinAmount = 0;var tiffinPrice = 0;
             var tiffinType = $('#tiffinType').val();
             var foodPreference = $('#foodPreference').val();
             var pendingBill = $('#pendingAmount').val();
+            messageFormat = messageFormat.replace("[pendingAmount]", pendingBill);
+            var totalTiffin = $("input[name='days[]']:checked").length;
+            messageFormat = messageFormat.replace("[totalTiffin]", totalTiffin);
             $.each($("input[name='days[]']:checked"), function() {
               var a = new Date($(this).val());
               var selectedDay = weekday[a.getDay()];
               tiffinPrice = (tiffinType==="half") ? 45 : 60;
               if(foodPreference=='nonveg' && selectedDay=='Friday'){
                 tiffinPrice = (tiffinType==="half") ? 60 : 75;
+                nonvegTiffinDates.push(a.getDate());
+              }else{
+                vegTiffinDates.push(a.getDate());
               }
               tiffinAmount += tiffinPrice;
             });
-            alert(tiffinAmount);
-
+            messageFormat = messageFormat.replace("[vegTiffinDates]", vegTiffinDates.join());
+            messageFormat = messageFormat.replace("[nonvegTiffinDates]", nonvegTiffinDates.join());
+            messageFormat = messageFormat.replace("[totalBillAmount]", tiffinAmount);
+            var grandTotal = parseInt(tiffinAmount) + parseInt(pendingBill);
+            messageFormat = messageFormat.replace("[grandTotal]", grandTotal);
+            $('#billModal').modal('show');
+            $('.createdBill').html(messageFormat);
         });
       });
     </script>
