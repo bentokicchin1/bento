bd281c6c6ed61ce77026d0a18e5bf71bb53117a5
Sujit magdum <magdumsujit@gmail.com>
2018-12-03 11:35:19 +0530

    fixes

--- config/constants.php
+++ config/constants.php
@@ -24,7 +24,7 @@ return [
     'ADDRESS_TYPE' => ['Home'=>'Home','Office'=>'Office'],
     'DEFAULT_HALF_VEG_TIFFIN' => ['veg_sabaji','chapati','others'],
     'DEFAULT_HALF_NONVEG_TIFFIN' => ['non_veg_sabaji','chapati','others'],
-    'LUNCH_ORDER_MAX_TIME' => '12:30 pm'
+    'LUNCH_ORDER_MAX_TIME' => '12:30 pm',
     'DINNER_ORDER_MAX_TIME' => '06:30 pm',
     'ORDER_TYPE_LUNCH' => 2,
     'ORDER_TYPE_DINNER' => 3,
