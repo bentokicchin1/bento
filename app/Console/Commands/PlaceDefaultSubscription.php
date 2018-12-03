39eabbe2211ecefd46774a426ea6b94f499a457f
Sujit magdum <magdumsujit@gmail.com>
2018-12-03 10:08:30 +0530

    fixes

--- app/Console/Commands/PlaceDefaultSubscription.php
+++ app/Console/Commands/PlaceDefaultSubscription.php
@@ -58,73 +58,70 @@ class PlaceDefaultSubscription extends Command
             foreach($monthlyUsers as $userDetails){
                 $notSubscribed = Subscription::where('user_id',$userDetails['id'])
                         ->where('updated_at','>=',$lastSunday)->get();
-                        echo "<pre/>";
-                        print_r($notSubscribed);
-                        exit;
-                // if(!empty($notSubscribed)){
-                //   $userId = $userDetails['id'];
-                //   $foodPreference = $userDetails['food_preference'];
-                //   $foodQuantity = $userDetails['tiffin_quantity'];
-                //   if(!empty($userDetails['address']) && !empty($foodPreference) && !empty($foodQuantity)) {
-                //     foreach ($userDetails['address'] as $address) {
-                //       $orderTypeId = $address['order_type_id'];
-                //   // $subscribedData = Subscription::select('order_type_id')->where('user_id',$userId)->get();
-                //   // if(!empty($subscribedData)){
-                //   //     foreach ($subscribedData as $subscriptionDetails) {
-                //           $defaultData = array();
-                //           // $orderTypeId = $subscriptionDetails->order_type_id;
-                //           $dishData = $this->subscriptionService->getDefaultDishList($orderTypeId);
-                //           if(!empty($dishData)){
-                //             foreach ($dishData as $day => $details) {
-                //               $orderTotalAmount = $details['orderTotalAmount'];
-                //               if(!empty($details['items'])){
-                //                 foreach ($details['items'] as $dishType => $dishDetails) {
-                //                   switch($foodPreference){
-                //                     case 'veg':
-                //                           $vegHalfDefault = config('constants.DEFAULT_HALF_VEG_TIFFIN');
-                //                           if(!in_array($dishType,$vegHalfDefault)  && $foodQuantity=='half'){
-                //                             $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
-                //                             unset($dishData[$day]['items'][$dishType]);
-                //                           }else if(isset($dishDetails['food_type']) && $dishDetails['food_type']=='nonveg'){
-                //                             $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
-                //                             unset($dishData[$day]['items'][$dishType]);
-                //                           }
-                //                       break;
-                //                     case 'nonveg':
-                //                           $nonVegHalfDefault = config('constants.DEFAULT_HALF_NONVEG_TIFFIN');
-                //                           if(date('l',strtotime($day))=='Wednesday' || date('l',strtotime($day))=='Friday'){
-                //                             if(!in_array($dishType,$nonVegHalfDefault) && $foodQuantity=='half'){
-                //                               $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
-                //                               unset($dishData[$day]['items'][$dishType]);
-                //                             }else if(isset($dishDetails['food_type']) && ($dishDetails['food_type']=='veg')) {
-                //                               $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
-                //                               unset($dishData[$day]['items'][$dishType]);
-                //                             }
-                //                           }else{
-                //                             $vegHalfDefault = config('constants.DEFAULT_HALF_VEG_TIFFIN');
-                //                             if(!in_array($dishType,$vegHalfDefault)  && $foodQuantity=='half'){
-                //                               $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
-                //                               unset($dishData[$day]['items'][$dishType]);
-                //                             }else if(isset($dishDetails['food_type']) && $dishDetails['food_type']=='nonveg'){
-                //                               $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
-                //                               unset($dishData[$day]['items'][$dishType]);
-                //                             }
-                //                           }
-                //                       break;
-                //                   }
-                //                 }
-                //               }
-                //               $dishData[$day]['orderTotalAmount'] = $orderTotalAmount;
-                //             }
-                //             $defaultData['subscriptionItems'] = json_encode($dishData);
-                //             $defaultData['orderTypeId'] = $orderTypeId;
-                //             $address = $this->addressService->getAddressByUserOrder($userId,$orderTypeId);
-                //             $defaultData['shippingAddressId'] = $address->id;
-                //             $result = $this->subscriptionService->processDefaultSubscription($defaultData,$userId);
-                //           }
-                //         }
-                //      }
-                //    }
+                if(empty($notSubscribed)){
+                  $userId = $userDetails['id'];
+                  $foodPreference = $userDetails['food_preference'];
+                  $foodQuantity = $userDetails['tiffin_quantity'];
+                  if(!empty($userDetails['address']) && !empty($foodPreference) && !empty($foodQuantity)) {
+                    foreach ($userDetails['address'] as $address) {
+                      $orderTypeId = $address['order_type_id'];
+                  // $subscribedData = Subscription::select('order_type_id')->where('user_id',$userId)->get();
+                  // if(!empty($subscribedData)){
+                  //     foreach ($subscribedData as $subscriptionDetails) {
+                          $defaultData = array();
+                          // $orderTypeId = $subscriptionDetails->order_type_id;
+                          $dishData = $this->subscriptionService->getDefaultDishList($orderTypeId);
+                          if(!empty($dishData)){
+                            foreach ($dishData as $day => $details) {
+                              $orderTotalAmount = $details['orderTotalAmount'];
+                              if(!empty($details['items'])){
+                                foreach ($details['items'] as $dishType => $dishDetails) {
+                                  switch($foodPreference){
+                                    case 'veg':
+                                          $vegHalfDefault = config('constants.DEFAULT_HALF_VEG_TIFFIN');
+                                          if(!in_array($dishType,$vegHalfDefault)  && $foodQuantity=='half'){
+                                            $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
+                                            unset($dishData[$day]['items'][$dishType]);
+                                          }else if(isset($dishDetails['food_type']) && $dishDetails['food_type']=='nonveg'){
+                                            $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
+                                            unset($dishData[$day]['items'][$dishType]);
+                                          }
+                                      break;
+                                    case 'nonveg':
+                                          $nonVegHalfDefault = config('constants.DEFAULT_HALF_NONVEG_TIFFIN');
+                                          if(date('l',strtotime($day))=='Wednesday' || date('l',strtotime($day))=='Friday'){
+                                            if(!in_array($dishType,$nonVegHalfDefault) && $foodQuantity=='half'){
+                                              $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
+                                              unset($dishData[$day]['items'][$dishType]);
+                                            }else if(isset($dishDetails['food_type']) && ($dishDetails['food_type']=='veg')) {
+                                              $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
+                                              unset($dishData[$day]['items'][$dishType]);
+                                            }
+                                          }else{
+                                            $vegHalfDefault = config('constants.DEFAULT_HALF_VEG_TIFFIN');
+                                            if(!in_array($dishType,$vegHalfDefault)  && $foodQuantity=='half'){
+                                              $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
+                                              unset($dishData[$day]['items'][$dishType]);
+                                            }else if(isset($dishDetails['food_type']) && $dishDetails['food_type']=='nonveg'){
+                                              $orderTotalAmount -= $dishData[$day]['items'][$dishType]['total_price'];
+                                              unset($dishData[$day]['items'][$dishType]);
+                                            }
+                                          }
+                                      break;
+                                  }
+                                }
+                              }
+                              $dishData[$day]['orderTotalAmount'] = $orderTotalAmount;
+                            }
+                            $defaultData['subscriptionItems'] = json_encode($dishData);
+                            $defaultData['orderTypeId'] = $orderTypeId;
+                            $address = $this->addressService->getAddressByUserOrder($userId,$orderTypeId);
+                            $defaultData['shippingAddressId'] = $address->id;
+                            $result = $this->subscriptionService->processDefaultSubscription($defaultData,$userId);
+                          }
+                        }
+                     }
+                   }
                 }
                 echo $result;
            }
