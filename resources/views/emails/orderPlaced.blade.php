13411a50f02679c4bece80b8b52d6114c9d687be
Shital Khilari <skhilari26@gmail.com>
2018-12-07 17:28:59 +0530

    fixes

--- resources/views/emails/orderPlaced.blade.php
+++ resources/views/emails/orderPlaced.blade.php
@@ -87,7 +87,7 @@
                                                             @if (is_array($order['orderDishes']))
                                                                 <p style="line-height: 1.5;font-size: 12px">Below is the summary of your order:- </p>
                                                                 @foreach($order['orderDishes'] as $orderItems)
-                                                                        <p style="line-height: 1.5;font-size: 12px">{{$orderItems['quantity']}} {{$orderItems['dishName']}} - {{$orderItems['totalPrice']}}</p>
+                                                                    <p style="line-height: 1.5;font-size: 12px">{{$orderItems['quantity']}} {{$orderItems['dishName']}} - {{$orderItems['totalPrice']}}</p>
                                                                 @endforeach
                                                             @endif
                                                         </p>
