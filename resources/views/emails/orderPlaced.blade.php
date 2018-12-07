32ffc2f9a033899657d803efad260b3f946a0d6d
Shital Khilari <skhilari26@gmail.com>
2018-12-07 17:03:59 +0530

    fixes

--- resources/views/emails/orderPlaced.blade.php
+++ resources/views/emails/orderPlaced.blade.php
@@ -89,7 +89,7 @@
                                                                 @foreach($order['orderDishes'] as $orderItems)
                                                                     @if (is_array($orderItems))
                                                                         @foreach($orderItems as $other)
-                                                                            <li>{{$other['quantity']}} {{$other['dishName']}} - {{$other['totalPrice']}}</li>
+                                                                            <!--<li>{{$other['quantity']}} {{$other['dishName']}} - {{$other['totalPrice']}}</li>-->
                                                                         @endforeach
                                                                     @else
                                                                         <li>{{$orderItems['quantity']}} {{$orderItems['dishName']}} - {{$other['totalPrice']}}</li>
