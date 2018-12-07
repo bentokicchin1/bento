9e8393705742de5d1a98ca5e69d0d648fde59847
Shital Khilari <skhilari26@gmail.com>
2018-12-07 18:13:12 +0530

    fixes

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -78,6 +78,7 @@ class DashboardController extends Controller
 //        }
         
         $orders = $this->orderService->getSingleOrderDetails(157);
+        foreach($orders['orderDishes'] as $key=>$orderItems){
         Mail::to('skhilari26@gmail.com')->send(new OrderPlaced($orders));
         
 //        return view('admin.dashboard', ['orders' => $orders,'orderList'=>$list]);
--- resources/views/emails/orderPlaced.blade.php
+++ resources/views/emails/orderPlaced.blade.php
@@ -83,16 +83,16 @@
                                                         <p style="line-height: 1.0;font-size: 12px">Your {{$order['order_type']['name']}} order dated {{date('jS M Y',strtotime($order['order_date']))}} is successfully placed.</p>
                                                         <br/>
                                                         <p style="line-height: 1.0;font-size: 12px">Order Total Amount -  {{$order['total_amount']}}Rs.</p>
-                                                        <p style="line-height: 1.5;font-size: 12px">Below is the summary of your order:- </p>
                                                         @if (is_array($order['orderDishes']))
+                                                            <p style="line-height: 0.5;font-size: 12px">Below is the summary of your order:- </p>
                                                             <ul>
                                                                 @foreach($order['orderDishes'] as $orderItems)
-                                                                    <li style="line-height: 1.5;font-size: 12px">{{$orderItems}}</li>
-                                                                    @if(is_array($orderItems))
-                                                                        @foreach($orderItems as $otherDishes)
-                                                                            <li style="line-height: 1.5;font-size: 12px">{{$otherDishes}}</li>
-                                                                        @endforeach
-                                                                    @endif
+                                                                    <li style="line-height: 1.5;font-size: 12px">{{$orderItems['quantity']}} {{$orderItems['dishName']}} - {{$orderItems['total_price']}}Rs.</li>
+                                                                    @foreach($orderItems as $otherDishes)
+                                                                        @if(is_array($otherDishes))
+                                                                            <li style="line-height: 1.5;font-size: 12px">{{$otherDishes['quantity']}} {{$otherDishes['dishName']}} - {{$otherDishes['total_price']}}Rs.</li>
+                                                                        @endif
+                                                                    @endforeach
                                                                 @endforeach
                                                             </ul>
                                                         @endif
