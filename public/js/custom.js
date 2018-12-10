e660c53e0ba9330050c90024a3cb5fdf2a8186c6
Shital Khilari <skhilari26@gmail.com>
2018-12-10 11:56:38 +0530

    bug fix

--- public/js/custom.js
+++ public/js/custom.js
@@ -171,7 +171,7 @@ $(document).ready(function(){
             mySelect.append(
                 $('<option></option>').val('').html('--- Select --- ')
             );
-        
+            console.log(area);
             $.each(areaLocations, function(key, val) {
                 console.log(val['area_id']+"   "+area);
                 if(val['area_id']==area){
