4b7ee4ac30a5b3cc646dc159b92f5f4fbc3344b0
Shital Khilari <skhilari26@gmail.com>
2018-12-10 15:16:32 +0530

    bug fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\OrderPlaced;
+use App\Mail\DefaultSubscriptionPlaced;
 use App\Model\User;
 use App\Model\Order;
 use App\Model\OrderItem;
