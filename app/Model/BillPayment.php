4f954a93e813dbaf89c1ffa8ff0aeca87d5560a0
Shital Khilari <skhilari26@gmail.com>
2019-01-07 11:59:45 +0530

    monthly bill generation

--- app/Model/BillPayment.php
+++ app/Model/BillPayment.php
@@ -35,6 +35,7 @@ class BillPayment extends Model
         if(!empty($billAmount)){
           $monthlyBillObj = new MonthlyBills;
           $monthlyBillObj->user_id = $user['id'];
+          $monthlyBillObj->invoice_id = $this->generateNewInvoiceId();
           $monthlyBillObj->bill_for_month = date('m',strtotime('first day of last month'));
           $monthlyBillObj->bill_for_year = date('Y');
           $monthlyBillObj->bill_date = date('Y-m-d');
@@ -49,5 +50,23 @@ class BillPayment extends Model
         $billObj->payment_date = date('Y-m-d');
         $billObj->save();
         DB::commit();
+    }
+    
+    
+    /*
+    * function sendGeneratedBills
+    * param $orders - Array of order details and cost of every order in previous month
+    */
+    public static function generateNewInvoiceId()
+    {
+        $record = MonthlyBills::latest()->first();
+        $expNum = explode('-', $record->invoice_id);
+        //check first day in a year
+        if ( date('l',strtotime(date('Y-01-01'))) ){
+            $nextInvoiceNumber = 'BTS'.date('Y').'-0001';
+        } else {
+            //increase 1 with last invoice number
+            $nextInvoiceNumber = 'BTS'.$expNum[0].'-'. $expNum[1]+1;
+        }
     }
 }
