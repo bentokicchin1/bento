3988b32dd6af4fe588aad71b39809ab8886d5a6f
Shital Khilari <skhilari26@gmail.com>
2019-01-07 12:08:38 +0530

    monthly bill generation

--- app/Model/MonthlyBills.php
+++ app/Model/MonthlyBills.php
@@ -3,9 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use DB;
 
 class MonthlyBills extends Model
 {
-    //
+    use SoftDeletes;
+    protected $table = 'bill_payments';
 }
