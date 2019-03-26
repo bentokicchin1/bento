cb80b5c4794592e55c4d46363921b1d3cff65c0c
Shital Khilari <skhilari26@gmail.com>
2019-03-26 12:26:21 +0530

    transactions

--- app/Http/Controllers/Transactions/PayuController.php
+++ app/Http/Controllers/Transactions/PayuController.php
@@ -19,12 +19,16 @@ class PayuController extends Controller
         $userDetails = array();
         $userDetails['key'] = config('constants.PAYU_MERCHANT_KEY');
         $userDetails['txnid'] = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
+        $userId = Auth::user()->id;
         $userDetails['firstname'] = Auth::user()->name;
         $userDetails['email'] = Auth::user()->email;
         $userDetails['phone'] = Auth::user()->mobile_number;
         $userDetails['productinfo'] = 'Tiffin Bill';
         $userDetails['amount'] = $amount;
-        $Data = Dish::all()->where('id', $id)->first();
+        $billData = BillPayment::all()->where('user_id', $userId)->orderBy('updated_at', 'desc')->first();
+        echo "<pre/>";
+        print_R($billData);
+        exit;
 //        $userDetails['udf1'] = $orderId;
         return Payment::make($userDetails, function ($then) {
             $then->redirectRoute('success');
